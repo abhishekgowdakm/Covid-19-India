5e51165766830c041969c1e03021fd13da8ab81d
Abhishekgowda KM <34267@ITCINFOTECH.COM>
2020-07-06 15:01:24 +0530

    Updates

---
+++ precache-manifest.321879314aebe1a3f103d2a372f9c0ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4fb6857ece77b03e0953c979609a29",
+    "url": "/Covid-19-India/index.html"
+  },
+  {
+    "revision": "3a93bc2cfef196ce433d",
+    "url": "/Covid-19-India/static/css/main.a9f32f63.chunk.css"
+  },
+  {
+    "revision": "db1ced0229bc7672058a",
+    "url": "/Covid-19-India/static/js/2.b19923a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid-19-India/static/js/2.b19923a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a93bc2cfef196ce433d",
+    "url": "/Covid-19-India/static/js/main.df527b8d.chunk.js"
+  },
+  {
+    "revision": "0a28f33e092e0725e703",
+    "url": "/Covid-19-India/static/js/runtime-main.58d39cf4.js"
+  },
+  {
+    "revision": "7c15d7bb7deb262ae954248b78d40215",
+    "url": "/Covid-19-India/static/media/covid-19.7c15d7bb.jpg"
+  }
+]);
